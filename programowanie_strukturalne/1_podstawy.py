69c24887d1631cd69fe5a9854acbcdba70f78dab
Venusian09 <kamil.debczak123@gmail.com>
2020-03-29 16:26:24 +0200

    cdv gr_4

--- programowanie_strukturalne/1_podstawy.py
+++ programowanie_strukturalne/1_podstawy.py
@@ -1,1 +1,72 @@
+'''
 print ("anna")
+
+#potegowanie
+pow=2**10
+print(pow)
+
+text="CDV"
+print(text*2)
+
+
+#pobieranie danych z klawiatury
+# + konkatenacja
+name=input("Podaj imię:")
+print("Twoje imie: " + name)
+
+surname=input("Podaj swoje nazwisko: ")
+print("Imię: " + name + ", naziwsko: " + surname)
+
+lenghtName=str(len(name))
+print("Długość imienia: " + lenghtName)
+
+lenSurname=len(surname)
+print(lenSurname)
+print(type(lenSurname))
+print(type(surname))
+
+
+imie naziwsko wiek
+format
+Imie i nazwisko: ..., wiek: ... lat
+
+imie=input("Podaj imie: ")
+nazwisko=input("Podaj nazwisko: ")
+wiek=input("Podaj wiek: ")
+print("Imie i nazwisko: " + imie + " " + nazwisko + "," + " wiek: " + wiek + " lat." )
+
+print("\n Podaj narodowosc:",end="")
+nationality=input();
+
+
+surname="Kowalski"
+firstLetter=surname[0]
+print(firstLetter)
+lastLetter=surname[len(surname)-1]
+print(lastLetter)
+
+
+#konwersja typów danych
+a="10"
+print(type(a))
+a=int(a)
+print(type(a))
+
+b=6
+print(type(b)) #int
+b/=2 #b=b/2
+print(type(b)) #float
+print(b) #3.0
+'''
+print()
+surname="Nowakowski"
+print(surname[0]) # N
+print(surname[0:2]) #No
+print(surname[-2]) #k
+print(surname[-2:]) #ki
+print(surname[:-2]) #Nowakows
+print(surname[:-2:2]) #Nwkw
+
+
+
+
